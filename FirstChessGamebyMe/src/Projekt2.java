457f82fb421ff44de47a0cc1a9f870e2012062f7
mkaya <mkaya@AP-5CG20441ZC.fritz.box>
2024-12-29 14:31:49 +0100

    First Changes, new GUI and Edge cases

--- FirstChessGamebyMe/src/Projekt2.java
+++ FirstChessGamebyMe/src/Projekt2.java
@@ -5,16 +5,16 @@ public class Projekt2 {
 	public static void main(String[] args) {
 
 		String[][] ChessField_First = { 
-				{ "X", "01", "02", "03", "04", "05", "06", "07", "08", "X" },
-				{ "1", "ST", "SS", "SL", "SQ", "SK", "SL", "SS", "ST", "1" },
-				{ "2", "SB", "SB", "SB", "SB", "SB", "SB", "SB", "SB", "2" },
-				{ "3", "XX", "XX", "XX", "XX", "XX", "XX", "XX", "XX", "3" },
-				{ "4", "XX", "XX", "XX", "XX", "XX", "XX", "XX", "XX", "4" },
-				{ "5", "XX", "XX", "XX", "XX", "XX", "XX", "XX", "XX", "5" },
-				{ "6", "XX", "XX", "XX", "XX", "XX", "XX", "XX", "XX", "6" },
-				{ "7", "WB", "WB", "WB", "WB", "WB", "WB", "WB", "WB", "7" },
-				{ "8", "WT", "WS", "WL", "WK", "WQ", "WL", "WS", "WT", "8" },
-				{ "X", "01", "02", "03", "04", "05", "06", "07", "08", "X" }, };
+				{ "XX", "01", "02", "03", "04", "05", "06", "07", "08", "XX" },
+				{ "01", "ST", "SS", "SL", "SQ", "SK", "SL", "SS", "ST", "01" },
+				{ "02", "SB", "SB", "SB", "SB", "SB", "SB", "SB", "SB", "02" },
+				{ "03", "XX", "XX", "XX", "XX", "XX", "XX", "XX", "XX", "03" },
+				{ "04", "XX", "XX", "XX", "XX", "XX", "XX", "XX", "XX", "04" },
+				{ "05", "XX", "XX", "XX", "XX", "XX", "XX", "XX", "XX", "05" },
+				{ "06", "XX", "XX", "XX", "XX", "XX", "XX", "XX", "XX", "06" },
+				{ "07", "WB", "WB", "WB", "WB", "WB", "WB", "WB", "WB", "07" },
+				{ "08", "WT", "WS", "WL", "WK", "WQ", "WL", "WS", "WT", "08" },
+				{ "XX", "01", "02", "03", "04", "05", "06", "07", "08", "XX" }, };
 		int[] from_X = new int[2];
 		int[] to_Y = new int [2];
 		System.out.println("Funktionsweise: Man gibt zuerst die Koordinaten von der zu bewegenden Figur ein, erst in der Vertikalen dannach die Horizontale.!");
@@ -53,11 +53,39 @@ public class Projekt2 {
 					to_Y[i] = 0;
 				}
 			}
+			for (int k = 0;k <= newField.length ;k++) {
+				if (k < newField.length - 1) {
+				System.out.print("—————");
+				}
+				if (k == newField.length) {
+					System.out.print("————");
+				}
+			}
 			for (int i = 0; i < newField.length; i++) {
 				System.out.println();
+				System.out.print("|");
 				for (int j = 0; j < newField[0].length; j++) {
 					System.out.print(newField[i][j]);
+					if (j < 9) {
 					System.out.print(" | ");
+					}
+					if (j == 9) {
+						System.out.print("|");
+					}
+				}
+				System.out.println();
+				if (i < 9) {
+				for (int k = 0;k < newField.length;k++) {
+					System.out.print(" ——  ");
+				} 
+				}
+			}
+			for (int k = 0;k <= newField.length ;k++) {
+				if (k < newField.length - 1) {
+				System.out.print("—————");
+				}
+				if (k == newField.length) {
+					System.out.print("————");
 				}
 			}
 		}
@@ -66,6 +94,22 @@ public class Projekt2 {
 	public static String[][] ChessField_Move(String[][] ChessField_current, int[] from_X, int[] to_Y) {
 		String MyFigure = ChessField_current[from_X[0]][from_X[1]];
 		String NewPosMyFigure = ChessField_current[to_Y[0]][to_Y[1]];
+		
+		if (ChessField_current[from_X[0]][from_X[1]].equals("XX")) { // Check for selected free Field
+			System.out.println("You can´t attak/move a free Field!");
+			return ChessField_current;
+		}
+		
+		for (int i = 0; i < 2;i++) { //Check for Field between 1-8
+			if (from_X[i] >= 9 || from_X[i] <= 0) {
+				System.out.println("Invalid Field, Choose a Field between 1-8!");
+				return ChessField_current;
+			}
+			if (to_Y[i] >= 9 || to_Y[i] <= 0) {
+				System.out.println("Invalid Field, Choose a Field between 1-8!");
+				return ChessField_current;
+			}
+		}
 		// OWN FIGURE ATTACK BLOCK
 		if (MyFigure.charAt(0) == NewPosMyFigure.charAt(0)) {
 			System.out.println("You can´t attack your own Figure!");
@@ -456,6 +500,7 @@ public class Projekt2 {
 							}
 						}
 					}
+					return ChessField_current_lokal;
 				}
 				if (from_X[0] - to_Y[0] == 1 && from_X[1] - to_Y[1] == -1) {
 					for (int i = 0; i < ChessField_current_lokal.length; i++) {
@@ -469,6 +514,7 @@ public class Projekt2 {
 							}
 						}
 					}
+					return ChessField_current_lokal;
 				}
 				if (from_X[0] - to_Y[0] == -1 && from_X[1] - to_Y[1] == 1) {
 					for (int i = 0; i < ChessField_current_lokal.length; i++) {
@@ -482,6 +528,7 @@ public class Projekt2 {
 							}
 						}
 					}
+					return ChessField_current_lokal;
 				}
 				if (from_X[0] - to_Y[0] == -1 && from_X[1] - to_Y[1] == -1) {
 					for (int i = 0; i < ChessField_current_lokal.length; i++) {
@@ -495,6 +542,7 @@ public class Projekt2 {
 							}
 						}
 					}
+					return ChessField_current_lokal;
 				}
 			}
 			// Movement upwards and downwards
@@ -511,6 +559,7 @@ public class Projekt2 {
 							}
 						}
 					}
+					return ChessField_current_lokal;
 				} else if (from_X[0] - to_Y[0] == -1) {
 					for (int i = 0; i < ChessField_current_lokal.length; i++) {
 						for (int j = 0; j < ChessField_current_lokal[0].length; j++) {
@@ -523,10 +572,12 @@ public class Projekt2 {
 							}
 						}
 					}
+					return ChessField_current_lokal;
 				} else {
 					System.out.println("Field not free!");
+					return ChessField_current;
 				}
-			}
+			}	
 			// Movement to left and right
 			if (YAchse == 0 && XAchse == 1) {
 				if (from_X[1] - to_Y[1] == 1) {
@@ -541,6 +592,7 @@ public class Projekt2 {
 							}
 						}
 					}
+					return ChessField_current_lokal;
 				}
 				if (from_X[1] - to_Y[1] == -1) {
 					for (int i = 0; i < ChessField_current_lokal.length; i++) {
@@ -554,11 +606,20 @@ public class Projekt2 {
 							}
 						}
 					}
+					return ChessField_current_lokal;
+				}
+				else {
+					System.out.println("Field not free!");
+					return ChessField_current;
 				}
 			}
-
-			return ChessField_current_lokal;
+			
+			else {
+				System.out.println("Invalid Move, try again!");
+				return ChessField_current;
+			}
 		}
+
 
 		// KNIGHT MOVEMENT AND ATTACK
 		if (ChessField_current[from_X[0]][from_X[1]] == "SS" || ChessField_current[from_X[0]][from_X[1]] == "WS") {
